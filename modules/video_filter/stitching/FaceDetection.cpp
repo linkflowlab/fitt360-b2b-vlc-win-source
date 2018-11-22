1e0b8db61120c3628e42232689ab5b83a3e4fd56
Aiden Kim <aiden@linkflow.co.kr>
2018-11-23 04:32:19 +0900

    Removed unnecessary headers in the code

--- modules/video_filter/stitching/FaceDetection.cpp
+++ modules/video_filter/stitching/FaceDetection.cpp
@@ -25,7 +25,6 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
 
 #include "LFSecurity.h"
 #include "FaceDetection.h"
