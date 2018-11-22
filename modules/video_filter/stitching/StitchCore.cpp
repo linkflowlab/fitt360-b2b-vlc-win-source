8bb29bfa882b002444889caafd978d0e62b1aa38
Aiden Kim <aiden@linkflow.co.kr>
2018-11-23 04:32:19 +0900

    Removed unnecessary headers in the code

--- modules/video_filter/stitching/StitchCore.cpp
+++ modules/video_filter/stitching/StitchCore.cpp
@@ -10,15 +10,8 @@
 
 #include <iostream> 
 
-#include "opencv2/opencv_modules.hpp"
-#include "opencv2/core/utility.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/stitching/detail/autocalib.hpp"
 #include "opencv2/stitching/detail/matchers.hpp"
 #include "opencv2/stitching/detail/seam_finders.hpp"
-#include "opencv2/stitching/detail/util.hpp"
-#include "opencv2/stitching/detail/warpers.hpp"
 #include "opencv2/stitching/warpers.hpp"
 
 #include "LFSecurity.h"
