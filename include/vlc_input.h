b145eedb5d135fc6b7de50a72b24dd937a42b3f4
Aiden Kim <aiden@linkflow.co.kr>
2018-12-10 03:43:10 +0900

    Changed prefix format text for recording

--- include/vlc_input.h
+++ include/vlc_input.h
@@ -231,7 +231,7 @@ struct input_thread_t
  * Record prefix string.
  * TODO make it configurable.
  */
-#define INPUT_RECORD_PREFIX "vlc-record-%Y-%m-%d-%Hh%Mm%Ss-$ N-$ p"
+#define INPUT_RECORD_PREFIX "Record$ p%Y%m%d_%H%M%S"
 
 /*****************************************************************************
  * Input events and variables
